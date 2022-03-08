412563bb10df7dc6177d05c55525efdf754ebafa
Jeon-jk123 <91730869+Jeon-jk123@users.noreply.github.com>
2022-03-08 16:04:32 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,11 @@
+jk1=localStorage.getItem("v1");
+jk2=localStorage.getItem("v2");
+
+score1=0;
+score2=0;
+document.getElementById("name_1").innerHTML= jk1 +" : ";
+document.getElementById("name_2").innerHTML= jk2 +" : ";
+document.getElementById("score_1").innerHTML=score1;
+document.getElementById("score_2").innerHTML=score2;
+document.getElementById("question").innerHTML= name_1;
+document.getElementById("answer").innerHTML= name_2;
